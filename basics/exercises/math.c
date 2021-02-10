6b511fea911404f3db3fadb0165e26543b8c257c
kasplars <kasp.lars@gmail.com>
2021-02-10 10:54:27 +0100

    Initial commit

---
+++ basics/exercises/math.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+#include<complex.h>
+
+int main(){
+	double val = tgamma(5);
+	printf("Gamma of 5 is %g\n",val);
+return 0;
+}
---
+++ basics/hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+#define PI 3.1415927
+
+int main(){
+	printf("hello\n");
+return 0;}
