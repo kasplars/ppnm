3d992230620268510de69172872c0b1d9c5c0d8d
kasplars <kasp.lars@gmail.com>
2021-04-29 10:23:52 +0200

    Part B of minimization homework done. Needs prettifying.

--- homework/minimization/main.c
+++ homework/minimization/main.c
@@ -75,6 +75,65 @@ void himmelblau(gsl_vector * vars, double * fx) {
 	* fx = pow(x*x+y-11,2)+pow(x+y*y-7,2);
 }
 
+void higgs(gsl_vector * vars, double * fx) {
+	int m = 30, n = 3;
+	double breitwigner; *fx = 0;
+	
+	FILE * data = fopen("higgsdata.txt","r");
+	gsl_matrix * datmat = gsl_matrix_alloc(m,n);
+	gsl_matrix_fscanf(data,datmat);
+	
+	double mass = gsl_vector_get(vars,0);
+	double Gamma = gsl_vector_get(vars,1);
+	double A = gsl_vector_get(vars,2);
+	double E, sigmai, dsigmai;
+	
+	for (int i = 0; i < m; i++) {
+		E = gsl_matrix_get(datmat,i,0);
+		sigmai = gsl_matrix_get(datmat,i,1);
+		dsigmai = gsl_matrix_get(datmat,i,2);
+		breitwigner = A / (pow(E-mass,2)+pow(Gamma,2)/4);
+		* fx += pow(breitwigner-sigmai,2)/pow(dsigmai,2);
+	}
+	
+	gsl_matrix_free(datmat);
+	fclose(data);
+}
+
+void higgsplot(gsl_vector * x) {
+	FILE * data = fopen("higgsdata.txt","r");
+	FILE * plotfile = fopen("plotdata.txt","w");
+	gsl_matrix * datmat = gsl_matrix_alloc(30,3);
+	gsl_matrix_fscanf(data,datmat);
+	
+	double mass = gsl_vector_get(x,0);
+	double Gamma = gsl_vector_get(x,1);
+	double A = gsl_vector_get(x,2);
+	double E, sigmai, dsigmai;
+	
+	fprintf(plotfile,"#index 0\n");
+	
+	for (int i = 0; i < 30; i++) {
+		E = gsl_matrix_get(datmat,i,0);
+		sigmai = gsl_matrix_get(datmat,i,1);
+		dsigmai = gsl_matrix_get(datmat,i,2);
+		fprintf(plotfile,"%10g %10g %10g\n",E,sigmai,dsigmai);
+	}
+	
+	fprintf(plotfile,"\n\n\n#index 1\n");
+	double Ein = gsl_matrix_get(datmat,0,0);
+	double Efin = gsl_matrix_get(datmat,29,0);
+	int numpoints = 300;
+	double h = (Efin - Ein) / (numpoints - 1.0);
+	for (int i = 0; i < numpoints; i++) {
+		fprintf(plotfile,"%10g %10g\n",Ein+h*i,A/(pow((Ein+h*i)-mass,2)+pow(Gamma,2)/4));
+	}
+	
+	gsl_matrix_free(datmat);
+	fclose(plotfile);
+	fclose(data);
+}
+
 int qnewton(void (*f)(gsl_vector * x, double * fx), gsl_vector * x, double acc) {
 	int n = x->size, numsteps = 0; double alpha;
 	double epsilon = 1e-6;
@@ -133,9 +192,24 @@ int main() {
 	steps = qnewton(himmelblau,y,acc);
 	vector_print("Minimum of Himmelblau's function = ",y);
 	printf("within accuracy %10g\n",acc);
-	printf("number of steps %i\n",steps);
+	printf("number of steps %i\n\n\n",steps);
 	
+	acc = 0.00001;
+	gsl_vector * z = gsl_vector_alloc(3);
+	double mass = 125.0, width = 2.09, scale = 2.0;
+	gsl_vector_set(z,0,mass);
+	gsl_vector_set(z,1,width);
+	gsl_vector_set(z,2,scale);
+	steps = qnewton(higgs,z,acc);
+	vector_print("Minimum of Higgs in the format [E,Gamma,A]^T = ",z);
+	printf("within accuracy %10g\n",acc);
+	printf("number of steps %i\n",steps);
+	higgsplot(z);
+	
+	
+	gsl_vector_free(x);
 	gsl_vector_free(y);
+	gsl_vector_free(z);
 		
 return 0;
 }
